4dbdeb5be201692b88eae7e6fcaed63f9ba26d09
untiwe <untiwe@gmail.com>
2023-02-05 17:54:28 +0300

    add del time image main.tar

--- BackEnd/updateserver.ps1
+++ BackEnd/updateserver.ps1
@@ -2,9 +2,9 @@ $pathFolder = "E:\arhive\Web\adviser\BackEnd"
 cd $pathFolder
 docker build -f ./Main/dockerfile -t main:latest .;
 docker save -o main.tar main;
-pscp -pw ${Env:WebUserPwd} main.tar  ${Env:WebUserName}@${Env:WebUserIP}:"/web-root/"
-plink -batch -ssh ${Env:WebUserName}@${Env:WebUserIP} -pw ${Env:WebUserPwd} "bash /web-root/updateserver.sh"
-
+pscp -pw ${Env:WebUserPwd} main.tar  ${Env:WebUserName}@${Env:WebUserIP}:"/web-root/";
+plink -batch -ssh ${Env:WebUserName}@${Env:WebUserIP} -pw ${Env:WebUserPwd} "bash /web-root/updateserver.sh";
+del main.tar;
 
 Write-Host -NoNewLine 'Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
