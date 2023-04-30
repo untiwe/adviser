bdc0bd49e5f7fb34569af4dd0e6614e0e78f8be1
untiwe <untiwe@gmail.com>
2023-04-30 22:11:00 +0300

    update script for updateserver

--- BackEnd/updateserver.ps1
+++ BackEnd/updateserver.ps1
@@ -1,10 +1,28 @@
-$pathFolder = "E:\arhive\Web\adviser\BackEnd"
-cd $pathFolder
-docker build -f ./Main/dockerfile -t main:latest .;
-docker save -o main.tar main;
-pscp -pw ${Env:WebUserPwd} main.tar  ${Env:WebUserName}@${Env:WebUserIP}:"/web-root/";
-plink -batch -ssh ${Env:WebUserName}@${Env:WebUserIP} -pw ${Env:WebUserPwd} "bash /web-root/updateserver.sh";
-del main.tar;
 
-Write-Host -NoNewLine 'Press any key to continue...';
+$value = Read-Host "1: update main
+2: update todo
+"
+if ( 1 -eq $value )
+{
+    $ServiseName = "main"
+}
+elseif ( 2 -eq $value )
+{
+    $ServiseName = "todo"
+}
+else
+{
+    echo "Couldn't read value"
+}
+$FolderServiseName = $ServiseName.substring(0,1).toupper()+$ServiseName.substring(1).tolower()   
+$pathFolder = "E:\arhive\Web\adviser\BackEnd"
+cd $pathFolder;
+
+docker build -f ./${FolderServiseName}/dockerfile -t ${ServiseName}:latest .;
+docker save -o "${ServiseName}.tar" $ServiseName;
+pscp -pw ${Env:WebUserPwd} "${ServiseName}.tar"  ${Env:WebUserName}@${Env:WebUserIP}:"/web-root/";
+plink -batch -ssh ${Env:WebUserName}@${Env:WebUserIP} -pw ${Env:WebUserPwd} "bash /web-root/updateserver.sh $ServiseName";
+del "${ServiseName}.tar";
+
+Write-Host -NoNewLine "Coplete update ${ServiseName}. Press any key to continue...";
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
