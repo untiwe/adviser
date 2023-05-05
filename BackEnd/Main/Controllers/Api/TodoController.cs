be1c42eec30703f68f0a4422504f4d7adceec5af
untiwe <untiwe@gmail.com>
2023-05-05 20:59:58 +0300

    del linq add SqlQuery

--- BackEnd/Main/Controllers/Api/TodoController.cs
+++ BackEnd/Main/Controllers/Api/TodoController.cs
@@ -1,5 +1,8 @@
 ﻿using Grpc.Net.Client;
+using Main.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Todo;
 
 namespace Main.Controllers.Api
@@ -10,21 +13,33 @@ namespace Main.Controllers.Api
 
     {
         private readonly string _todoUrl;
-        public TodoController()
+        private readonly DBContext _dbContext;
+        public TodoController(DBContext dbContext)
         {
+            _dbContext = dbContext;
+
             _todoUrl = Environment.GetEnvironmentVariable("TodoServiseURL");
             if (string.IsNullOrEmpty(_todoUrl))
             {
                 throw new ArgumentException("Нет URL для подключения к сервису");
             }
         }
+
+        [Authorize]
         [HttpPost]
-        public async Task<IActionResult> Index(string name)
+        public async Task<IActionResult> AddnewTodo(string text)
         {
+            var userid = GetUserId();
             using var channel = GrpcChannel.ForAddress(_todoUrl);
             var client = new TodoService.TodoServiceClient(channel);
-            var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+            var reply = await client.AddnewTodoAsync(new AddnewTodoRequest { Userid = userid, Text = text});
             return Ok(reply);
+        }
+
+        private int GetUserId()
+        {
+            FormattableString requrstSQL = $"SELECT \"Users\".\"Id\" from public.\"Users\" WHERE \"Login\" = {HttpContext.User.Identity.Name}";
+            return _dbContext.Database.SqlQuery<int>(requrstSQL).ToList().First();
         }
     }
 }
