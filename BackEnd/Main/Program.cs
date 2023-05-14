08b2cdee8527c82914a44c7b540681be17b4ef7b
untiwe <untiwe@gmail.com>
2023-05-14 21:24:03 +0300

    move some *add and *use extensions from Program.cs

--- BackEnd/Main/Program.cs
+++ BackEnd/Main/Program.cs
@@ -3,6 +3,7 @@ using Main.Models;
 using Main.Options;
 using Main.Services;
 using Main.Services.Interfaces;
+using Main.SevrviceExtendions;
 using Main.Validation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -19,97 +20,18 @@ internal class Program
             options.ListenAnyIP(5000);
         });
 
-        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-        {
-
-            options.RequireHttpsMetadata = false;
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                // укзывает, будет ли валидироваться издатель при валидации токена
-                ValidateIssuer = true,
-                // строка, представляющая издателя
-                ValidIssuer = JWTOptions.ISSUER,
-
-                // будет ли валидироваться потребитель токена
-                ValidateAudience = true,
-                // установка потребителя токена
-                ValidAudience = JWTOptions.AUDIENCE,
-                // будет ли валидироваться время существования
-                ValidateLifetime = true,
-
-                // установка ключа безопасности
-                IssuerSigningKey = JWTOptions.GetSymmetricSecurityKey(),
-                // валидация ключа безопасности
-                ValidateIssuerSigningKey = true
-            };
-            options.Events = new JwtBearerEvents
-            {
-                //убираем требование "Bearer " с коротого начинается токен по умолчнию
-                OnMessageReceived = context =>
-                {
-                    string authorizationToken = context.Request.Headers["Authorization"];
-                    if (string.IsNullOrEmpty(authorizationToken))
-                        context.NoResult();
-                    else
-                        context.Token = authorizationToken;
-
-                    return Task.CompletedTask;
-                }
-            };
-
-        });
-
+        builder.Services.AddAuthenticationExtension();
 
 
         builder.Services.AddAuthorization();
         builder.Services.AddControllers();
-        builder.Services.AddSwaggerGen(c =>
-        {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "AdviserAPI", Version = "v1" });
-            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            {
-                Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-                Name = "Authorization",
-                In = ParameterLocation.Header,
-                Type = SecuritySchemeType.ApiKey
+        builder.Services.AddSwaggerExtension();
 
-            });
-
-            c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-            {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                },
-                Scheme = "oauth2",
-                Name = "Bearer",
-                In = ParameterLocation.Header,
-
-            },
-            new List<string>()
-        }
-            });
-        });
-
-
-        var ConnectionString = Environment.GetEnvironmentVariable("ConnectionString");
-        if (ConnectionString == null)
-            ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-        builder.Services.AddDbContext<DBContext>(options => options.UseNpgsql(ConnectionString));
+        builder.Services.AddDbContextExtension(builder.Configuration);
 
         builder.Services.AddScoped<IAuth, Auth>();
         builder.Services.AddScoped<IPasswordManager, PasswordManager>();
-        builder.Services.AddCors(options =>
-
-                options.AddDefaultPolicy(
-                    policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
-                    )
-                );
+        builder.Services.AddCorsExtension();
         builder.Services.AddValidatorsFromAssemblyContaining<CustomerValidator>();
         var app = builder.Build();
 
@@ -124,16 +46,7 @@ internal class Program
         app.UseDeveloperExceptionPage();
 
 
-        using (var scope = app.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-
-            var context = services.GetRequiredService<DBContext>();
-            if (context.Database.GetPendingMigrations().Any())
-            {
-                context.Database.Migrate();
-            }
-        }
+        app.UseDataMaseMigrations();
 
         app.UseStaticFiles();
         app.UseRouting();
---
+++ BackEnd/Main/SevrviceExtendions/AppExtensions.cs
@@ -0,0 +1,116 @@
+﻿//using Microsoft.Extensions.DependencyInjection;
+
+using Main.Models;
+using Main.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace Main.SevrviceExtendions
+
+{
+    public static class AppExtensions
+    {
+        public static IServiceCollection AddSwaggerExtension(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AdviserAPI", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                    {
+                        {
+                            new OpenApiSecurityScheme                    {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                },
+                                Scheme = "oauth2",
+                                Name = "Bearer",
+                                In = ParameterLocation.Header,
+
+                            },
+                            new List<string>()
+                        }
+                    });
+            });
+            return services;
+            //services.AddTransient<TimeService>();
+        }
+        public static IServiceCollection AddAuthenticationExtension(this IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    // укзывает, будет ли валидироваться издатель при валидации токена
+                    ValidateIssuer = true,
+                    // строка, представляющая издателя
+                    ValidIssuer = JWTOptions.ISSUER,
+
+                    // будет ли валидироваться потребитель токена
+                    ValidateAudience = true,
+                    // установка потребителя токена
+                    ValidAudience = JWTOptions.AUDIENCE,
+                    // будет ли валидироваться время существования
+                    ValidateLifetime = true,
+
+                    // установка ключа безопасности
+                    IssuerSigningKey = JWTOptions.GetSymmetricSecurityKey(),
+                    // валидация ключа безопасности
+                    ValidateIssuerSigningKey = true
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    //убираем требование "Bearer " с коротого начинается токен по умолчнию
+                    OnMessageReceived = context =>
+                    {
+                        string authorizationToken = context.Request.Headers["Authorization"];
+                        if (string.IsNullOrEmpty(authorizationToken))
+                            context.NoResult();
+                        else
+                            context.Token = authorizationToken;
+
+                        return Task.CompletedTask;
+                    }
+                };
+
+            });
+
+            return services;
+        }
+        public static IServiceCollection AddCorsExtension(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+
+                options.AddDefaultPolicy(
+                    policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+                    )
+                );
+
+            return services;
+        }
+        public static IServiceCollection AddDbContextExtension(this IServiceCollection services, IConfiguration config)
+        {
+            var ConnectionString = Environment.GetEnvironmentVariable("ConnectionString");
+            if (ConnectionString == null)
+                ConnectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DBContext>(options => options.UseNpgsql(ConnectionString));
+
+            return services;
+        }
+    }
+}
